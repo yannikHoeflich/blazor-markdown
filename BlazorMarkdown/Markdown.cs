1367ed617a65e42f66e148950a46192c7770ea2f
Yannik Hoeflich <ygidyyy@gmail.com>
2022-09-17 16:51:08 +0200

    Marked Markdown component as obsolete

--- BlazorMarkdown/Markdown.cs
+++ BlazorMarkdown/Markdown.cs
@@ -3,12 +3,14 @@
 using Markdig;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Rendering;
+using System;
 
 namespace BlazorMarkdown
 {
     /// <summary>
     /// A Component for displaying Markdown.
     /// </summary>
+    [Obsolete]
     public class Markdown : MarkdownFile {
     }
 }
